d98a22c1e07e46e9bc6a9609550a1460334986c3
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-09-10 22:10:31 -0400

    Created data folder, and entered an entry data

---
+++ data/videos.js
@@ -0,0 +1,8 @@
+
+//TODO: figure out if this should be a list of arrays or list of 2D arrays
+let videos = [
+    [
+        ["https://www.youtube.com/watch?v=P00HMxdsVZI", "0:40"],
+        ["https://www.youtube.com/watch?v=b8m9zhNAgKs", "0:26"]
+    ]
+]
