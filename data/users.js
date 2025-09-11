5e9130af5615ebae8887e3db82b5fd57d4a3ec26
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-09-11 09:15:04 -0400

    Added comments.js and users,js in the data folder

---
+++ data/users.js
@@ -0,0 +1,20 @@
+let users = [
+    {
+        id: 0,
+        username: "Bogard",
+        password: "UHYtgr3edxs",
+        email: "star@google.com"
+    },
+    {
+        id: 1,
+        username: "Domer",
+        password: "Jeffery",
+        email: "cerial@google.com"
+    },
+    {
+        id: 2,
+        username: "password",
+        password: "us3rn@m3",
+        email: "duckduck@goose.com"
+    }
+]
